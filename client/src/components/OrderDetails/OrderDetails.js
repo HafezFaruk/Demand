7f29cd4e9fc83723ea5fe2498f060b8e3c885477
HafezFaruk <hmomarfaruk1454@gmail.com>
2022-03-25 19:36:55 +0600

    admin brunch create

--- client/src/components/OrderDetails/OrderDetails.js
+++ client/src/components/OrderDetails/OrderDetails.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const OrderDetails = () => {
     return (
-        <div>
-            <div></div>
-        </div>
+      <div>
+        <div>OrderDetails</div>
+      </div>
     );
 };
 
