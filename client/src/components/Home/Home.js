56f393f8447b3e28dca6fa4bd426130e03b4e273
ashrafulislampro <ashrafulislam1659@gmail.com>
2022-03-28 08:23:25 +0600

    API json file is completed

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Banner from '../Banner/Banner.js'
+import imgOff from '../../img/offer-card.jpg';
+import Banner from '../Banner/Banner.js';
 import CardItem from '../CardItem/CardItem.js';
 import MiniCard from '../MiniCard/MiniCard.js';
 import OfferCard from '../OfferCard/OfferCard.js';
 import ShopByCategory from '../Shop/ShopByCategory.js';
-import imgOff from '../../img/offer-card.jpg';
 
 
 const Home = () => {
@@ -25,3 +25,193 @@ const Home = () => {
 };
 
 export default Home;
+
+{/* <div className="bg-white shadow rounded overflow-hidden group">
+            <div className="relative">
+              <img
+                src="http://rafcart.rslahmed.com/assets/images/product1.jpg"
+                className="w-full"
+                alt=""
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
+                <a
+                  href="/#"
+                  className="text-white text-lg w-9 h-8 rounded-lg bg-myColor flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fa-solid fa-eye"></i>
+                </a>
+                <a
+                  href="/#"
+                  className="text-white text-lg w-9 h-8 rounded-lg bg-myColor flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fas fa-heart"></i>
+                </a>
+              </div>
+            </div>
+            <div className="pt-3 pb-3 px-4">
+              <a href="/#">
+                <h4 className="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition">
+                  madeline sofa
+                </h4>
+              </a>
+              <p className="capitalize font-medium text-sm mb-2 text-gray-800 hover:text-primary transition">
+                Fabric Sofa bed
+              </p>
+              <div className="flex items-baseline mb-1 space-x-2">
+                <p className="text-xl text-primary font-semibold">$120.00</p>
+                <p className="text-sm text-gray-400 line-through">$100.00</p>
+              </div>
+              <div className="flex items-center">
+                <div className="flex gap-1 text-sm text-yellow-400">
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                </div>
+                <div className="text-xs text-gray-500 ml-3">(160)</div>
+              </div>
+            </div>
+            <a
+              href="/#"
+              className="block w-full py-1 text-center text-white  bg-myColor border border-myColor rounded-b hover:bg-transparent hover:text-primary transition"
+            >
+              Add to card
+            </a>
+          </div>
+          <div className="bg-white shadow rounded overflow-hidden group">
+            <div className="relative">
+              <img
+                src="http://rafcart.rslahmed.com/assets/images/product12.jpg"
+                className="w-full"
+                alt=""
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
+                <a
+                  href="/#"
+                  className="text-white text-lg w-9 h-8 rounded-lg bg-myColor flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fa-solid fa-eye"></i>
+                </a>
+                <a
+                  href="/#"
+                  className="text-white text-lg w-9 h-8 rounded-lg bg-myColor flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fas fa-heart"></i>
+                </a>
+              </div>
+            </div>
+            <div className="pt-3 pb-3 px-4">
+              <a href="/#">
+                <h4 className="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition">
+                  black archie
+                </h4>
+              </a>
+              <p className="capitalize font-medium text-sm mb-2 text-gray-800 hover:text-primary transition">
+                black bedside
+              </p>
+              <div className="flex items-baseline mb-1 space-x-2">
+                <p className="text-xl text-primary font-semibold">$400.00</p>
+                <p className="text-sm text-gray-400 line-through">$380.00</p>
+              </div>
+              <div className="flex items-center">
+                <div className="flex gap-1 text-sm text-yellow-400">
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                </div>
+                <div className="text-xs text-gray-500 ml-3">(130)</div>
+              </div>
+            </div>
+            <a
+              href="/#"
+              className="block w-full py-1 text-center text-white bg-myColor border border-myColor rounded-b hover:bg-transparent hover:text-primary transition"
+            >
+              Add to card
+            </a>
+          </div>
+          <div className="bg-white shadow rounded overflow-hidden group">
+            <div className="relative">
+              <img
+                src="http://rafcart.rslahmed.com/assets/images/product8.jpg"
+                className="w-full"
+                alt=""
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
+                <a
+                  href="/#"
+                  className="text-white text-lg w-9 h-8 rounded-lg bg-myColor flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fa-solid fa-eye"></i>
+                </a>
+                <a
+                  href="/#"
+                  className="text-white text-lg w-9 h-8 rounded-lg bg-myColor flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fas fa-heart"></i>
+                </a>
+              </div>
+            </div>
+            <div className="pt-3 pb-3 px-4">
+              <a href="/#">
+                <h4 className="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition">
+                  bianco chair
+                </h4>
+              </a>
+              <p className="capitalize font-medium text-sm mb-2 text-gray-800 hover:text-primary transition">
+                fabric accent chair
+              </p>
+              <div className="flex items-baseline mb-1 space-x-2">
+                <p className="text-xl text-primary font-semibold">$60.00</p>
+                <p className="text-sm text-gray-400 line-through">$55.00</p>
+              </div>
+              <div className="flex items-center">
+                <div className="flex gap-1 text-sm text-yellow-400">
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star"></i>
+                  </span>
+                </div>
+                <div className="text-xs text-gray-500 ml-3">(170)</div>
+              </div>
+            </div>
+            <a
+              href="/#"
+              className="block w-full py-1 text-center text-white bg-myColor border border-myColor rounded-b hover:bg-transparent hover:text-primary transition"
+            >
+              Add to card
+            </a>
+          </div> */}
